974f2032a968e3c7b5ba75c4f250b8351734e7ae
Karol Bajko <karol.bajko@gmail.com>
2009-07-17 15:25:45 +0200

    Added 'clear' button

--- lib/searchable/helper.rb
+++ lib/searchable/helper.rb
@@ -7,8 +7,16 @@ module Searchable::Helper
       concat "\n"
       concat submit_tag(t('search'), :name => nil)
       concat "\n"
+      concat link_to(t('clear'), searchable_clear)
+      concat "\n"
     end
     concat "\n"
+  end
+
+  def searchable_clear
+    path_options = params.dup
+    path_options[:query] = nil
+    url_for(path_options)
   end
 
   def searchable_h(value, options={})
